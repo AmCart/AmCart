2162cf936a3353d97b10440cf8177eccc1ca8a3b
Sachin Bansal <sachin.bansal@nagarro.com>
2019-11-11 16:46:54 +0530

    Login Service

---
+++ JWTAuthentication/JWTAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        public LoginController(IConfiguration configuration)
+        {
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody]User login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(User userInfo)
+        {
+            //TODO: Make this configurable
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SecretKey"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(null, null, null, expires: DateTime.Now.AddMinutes(120),signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private User AuthenticateUser(User login)
+        {
+            User user = null;
+
+            //Validate the User Credentials  
+            //Demo Purpose, I have Passed HardCoded User Information  
+            //if (login.Username == "SachinBansal" && login.Password == "SachinBansal")
+            {
+                user = new User
+                {
+                    Username = login.Username,
+                    Password = login.Password,
+                    FirstName = "Sachin",
+                    LastName = "Bansal"
+                };
+            }
+            return user;
+        }
+    }
+}
